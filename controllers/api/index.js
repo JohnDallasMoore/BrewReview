c754e3b4f3b55f77039227f7aad0cce29e0a02c2
Michaela Brown <michaelarbrown4@gmail.com>
2023-06-22 21:05:39 -0500

    added base code for the api routes and added the code to the connection.js file, currently hardcoded until we add a .env file

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -6,3 +6,5 @@ const commentRoutes = require('./commentRoutes');
 router.use('/users', userRoutes);
 router.use('/posts', postRoutes);
 router.use('/comments', commentRoutes);
+
+module.exports = router;
