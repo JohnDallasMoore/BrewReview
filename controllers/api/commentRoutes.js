cd1be2c48d5a1d932f5b3c2b928717c81c200dbf
Michaela Brown <michaelarbrown4@gmail.com>
2023-06-26 20:18:02 -0500

    added login, logout, and new user routes

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -1,4 +1,16 @@
 const router = require('express').Router();
-const { Comment, Post, User } = require('../../models');
+const { Comment } = require('../../models');
+
+router.post('/', async (req, res) => {
+    try {
+      const newComment= await Comment.create({
+        ...req.body,
+     //   user_id: req.session.user_id,
+      });
+      res.status(200).json(newComment);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+});
 
 module.exports = router;
