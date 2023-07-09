f7a5c54f86538c5fc28c1c443df1dbe85813df34
Michaela Brown <michaelarbrown4@gmail.com>
2023-07-08 19:16:20 -0500

    fixed the postComment partial

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -4,11 +4,13 @@ const { Comment } = require('../../models');
 
 //create a new comment
 router.post('/', async (req, res) => {
+  const commentDate = new Date();
     try {
       const newComment= await Comment.create({
         ...req.body,
         user_id: req.session.user_id,
         post_id: req.session.post_id,
+        date: commentDate,
       });
       res.status(200).json(newComment);
     } catch (err) {
